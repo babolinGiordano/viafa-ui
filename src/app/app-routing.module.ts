5a171485c3e7497e687ecda431f359dfb6f6f5a9
Giordano Babolin <g.babolin@enerbrain.com>
2021-08-01 18:47:01 +0200

    Generate tutte le pagine necessarie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 import { DefaultComponent } from './layouts/default/default.component';
-import { PostsComponent } from './modules/posts/posts.component';
+import { ClientsComponent } from './modules/clients/clients.component';
+import { DriversComponent } from './modules/drivers/drivers.component';
+import { TravelsComponent } from './modules/travels/travels.component';
+import { TrucksComponent } from './modules/trucks/trucks.component';
 
 const routes: Routes = [
   {
@@ -14,8 +17,20 @@ const routes: Routes = [
         component: DashboardComponent
       },
       {
-        path: 'posts',
-        component: PostsComponent
+        path: 'clients',
+        component: ClientsComponent
+      },
+      {
+        path: 'drivers',
+        component: DriversComponent
+      },
+      {
+        path: 'trucks',
+        component: TrucksComponent
+      },
+      {
+        path: 'travels',
+        component: TravelsComponent
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,13 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { PostsComponent } from './modules/posts/posts.component';
+import { ClientsComponent } from './modules/clients/clients.component';
+import { DriversComponent } from './modules/drivers/drivers.component';
+import { TrucksComponent } from './modules/trucks/trucks.component';
+import { TravelsComponent } from './modules/travels/travels.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ClientsComponent, DriversComponent, TrucksComponent, TravelsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
